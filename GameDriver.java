5bfb87d0887e4dab65976a02d7961185b0913d5d
BogdanSotnikov <bogdans2789>
2025-01-10 10:35:48 -0600

    Working on display

--- GameDriver.java
+++ GameDriver.java
@@ -1,4 +1,12 @@
+import java.util.*;
+
 class GameDriver
 {
-  
+  public static void main(String[] args)
+  {
+    Text.hideCursor();
+    Text.clear();
+    Game.drawScreen();
+    Game.quit();
+  }
 }
